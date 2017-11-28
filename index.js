2369a55ee9be3d04d5e25edb9bd1fb0888055270
lutangar <johan.dufour@gmail.com>
2017-11-28 17:13:28 +0100

    expose function rather than object

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-module.exports = {
-  presets: [
-    [
-      require("@babel/preset-env"),
-      {
-        "targets": {
-          "node": "current"
-        },
-        "debug": false
-      }
+module.exports = function() {
+  return {
+    presets: [
+      [
+        require("@babel/preset-env"),
+        {
+          "targets": {
+            "node": "current"
+          },
+          "debug": false
+        }
+      ],
+      require("@babel/preset-react")
     ],
-    require("@babel/preset-react")
-  ],
-  ignore: ["node_modules"],
-  plugins: [
-    require("@babel/plugin-proposal-class-properties"),
-    require("@babel/plugin-proposal-object-rest-spread")
-  ]
-};
+    ignore: ["node_modules"],
+    plugins: [
+      require("@babel/plugin-proposal-class-properties"),
+      require("@babel/plugin-proposal-object-rest-spread")
+    ]
+  }
+}
