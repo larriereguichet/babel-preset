3a8f7ec15d53f32f11baf71024137301ccf62dc3
lutangar <johan.dufour@gmail.com>
2017-11-28 17:14:46 +0100

    remove ignore key

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ module.exports = function() {
       ],
       require("@babel/preset-react")
     ],
-    ignore: ["node_modules"],
     plugins: [
       require("@babel/plugin-proposal-class-properties"),
       require("@babel/plugin-proposal-object-rest-spread")
